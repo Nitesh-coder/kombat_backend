b0a41cb8564db9489a8f1b585e8ea7ca50bf5d03
Nitesh Ghimire <initesh100@gmail.com>
2024-08-06 14:52:17 +0545

    cors add

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const app = express()
+const cors  = require('cors')
+
+const data = [
+    {
+      name: 'XAUUSD',
+      invest: 50000
+    },
+    {
+      name: "WEB3",
+      invest: 23000
+    },
+    {
+      name: 'REACT',
+      invest: 40000
+    },
+    {
+      name: 'DREAMS',
+      invest: 12000
+    },
+    {
+      name: 'DREAMS',
+      invest: 12000
+    },
+    {
+      name: 'DREAMS',
+      invest: 12000
+    },
+    {
+      name: 'DREAMS',
+      invest: 12000
+    },
+  ]
+app.use(cors())
 
 app.get('/', (req,res)=>{
     res.send("hello world")
 })
 app.get('/api', (req,res)=>{
-    res.json({
-        name: "SELLS",
-        invest: 80005
-    })
+    res.json(data)
 })
 app.listen(3000)
